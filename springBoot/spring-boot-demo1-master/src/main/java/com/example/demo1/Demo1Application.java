b7de36de1c5df160a63a8b98c2cf1f6814517c6f
dengye <15976969404@163.com>
2019-11-13 11:15:39 +0800

    【3.4.0】 人事合同添加注解

--- springBoot/spring-boot-demo1-master/src/main/java/com/example/demo1/Demo1Application.java
+++ springBoot/spring-boot-demo1-master/src/main/java/com/example/demo1/Demo1Application.java
@@ -23,6 +23,7 @@ public class Demo1Application extends SpringBootServletInitializer {
 	public String home() {
 		Date now = new Date();
 		logger.info("url:{},time:{},", "/", now);
+		System.out.println("");
 		return "hello 朋友";
 	}
 
