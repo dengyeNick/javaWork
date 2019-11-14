91d1d936180b482c2d0ac3ea898c280fbdc664c0
dengye <15976969404@163.com>
2019-11-14 11:39:36 +0800

    【1.2.0】打印demo启动时间

--- springBoot/spring-boot-demo1-master/src/main/java/com/example/demo1/Demo1Application.java
+++ springBoot/spring-boot-demo1-master/src/main/java/com/example/demo1/Demo1Application.java
@@ -11,38 +11,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @SpringBootApplication
 @RestController
 public class Demo1Application extends SpringBootServletInitializer {
 
-	private static Logger logger = LoggerFactory.getLogger(Demo1Application.class);
+    private static Logger logger = LoggerFactory.getLogger(Demo1Application.class);
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-	@RequestMapping("/")
-	public String home() {
-		Date now = new Date();
-		logger.info("url:{},time:{},", "/", now);
-		return "hello 朋友";
-	}
+    @RequestMapping("/")
+    public String home() {
+        Date now = new Date();
+        logger.info("url:{},time:{},", "/", now);
+        return "hello 朋友";
+    }
 
-	@RequestMapping("/user/{id}")
-	public String getUser(@PathVariable("id") Integer id) {
-		if (id == null) {
-			throw new BusiException(500, "参数错误");
-		}
+    @RequestMapping("/user/{id}")
+    public String getUser(@PathVariable("id") Integer id) {
+        if (id == null) {
+            throw new BusiException(500, "参数错误");
+        }
 
-		Date now = new Date();
-		logger.info("url:{},time:{},id:{}", "/user/{id}", now, id);
-		return "user:...........";
-	}
+        Date now = new Date();
+        logger.info("url:{},time:{},id:{}", "/user/{id}", now, id);
+        return "user:...........";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
-	}
+    public static void main(String[] args) {
+        logger.info("demo1启动时间：" + sdf.format(new Date()));
+        SpringApplication.run(Demo1Application.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Demo1Application.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Demo1Application.class);
+    }
 }
